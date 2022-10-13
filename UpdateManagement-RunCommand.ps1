846a313b6413f2716adfdee146cdd31d4009b6ee
Rosario Vinoth <rosario.vinoth@gmail.com>
2022-10-14 00:52:26 +0530

    update

--- UpdateManagement-RunCommand.ps1
+++ UpdateManagement-RunCommand.ps1
@@ -1,31 +1,21 @@
 <#
 .SYNOPSIS
  Stop a service on an AzureRM using RunCommand
-
 .DESCRIPTION
   This script is intended to be run as a part of Update Management Pre/Post scripts. 
   It uses RunCommand to execute a PowerShell script to stop a service
-
 .PARAMETER SoftwareUpdateConfigurationRunContext
   This is a system variable which is automatically passed in by Update Management during a deployment.
 #>
-#requires -Modules ThreadJob
+
 param(
-    [Parameter(Mandatory)][string]$SoftwareUpdateConfigurationRunContext
+    [Parameter(Mandatory)][string]$SoftwareUpdateConfigurationRunContext,
+	[Parameter(Mandatory)][string]$RHEL_EUS_VERSION_ID
 )
 
-#region BoilerplateAuthentication
-#This requires a RunAs account
-$ServicePrincipalConnection = Get-AutomationConnection -Name 'AzureRunAsConnection'
+Connect-AzAccount -Identity
 
-Add-AzureRmAccount `
-    -ServicePrincipal `
-    -TenantId $ServicePrincipalConnection.TenantId `
-    -ApplicationId $ServicePrincipalConnection.ApplicationId `
-    -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint
-
-$AzureContext = Select-AzureRmSubscription -SubscriptionId $ServicePrincipalConnection.SubscriptionID
-#endregion BoilerplateAuthentication
+$ErrorActionPreference = "Stop"
 
 #If you wish to use the run context, it must be converted from JSON
 $context = ConvertFrom-Json  $SoftwareUpdateConfigurationRunContext
@@ -48,7 +38,7 @@ if (!$vmIds)
 
 #The script you wish to run on each VM
 $scriptBlock = @"
-Stop-Service -Name "AudioSvc"
+echo "$RHEL_EUS_VERSION_ID" > /etc/yum/vars/releasever
 "@
 $scriptPath = "$runID.ps1"
 #The cmdlet only accepts a file, so temporarily write the script to disk using runID as a unique name
@@ -67,11 +57,10 @@ $vmIds | ForEach-Object {
     $rg = $split[4];
     $name = $split[8];
     Write-Output ("Subscription Id: " + $subscriptionId)
-    $mute = Select-AzureRmSubscription -Subscription $subscriptionId
+    $mute = Set-AzContext -Subscription $subscriptionId
     Write-Output "Invoking command on '$($name)' ..."
-    $newJob = Start-ThreadJob -ScriptBlock { param($resourceGroup, $vmName, $scriptPath) Invoke-AzureRmVMRunCommand -ResourceGroupName $resourceGroup -Name $VmName -CommandId 'RunPowerShellScript' -ScriptPath $scriptPath} -ArgumentList $rg, $name, $fullPath
+    $newJob = Start-ThreadJob -ScriptBlock { param($resourceGroup, $vmName, $scriptPath) Invoke-AzVMRunCommand -ResourceGroupName $resourceGroup -VMName $VmName -CommandId 'RunShellScript' -ScriptPath $scriptPath} -ArgumentList $rg, $name, $fullPath
     $jobIDs.Add($newJob.Id)
-    
 }
 
 $jobsList = $jobIDs.ToArray()
